c28289558423a7f9aa50e2adfbd02c6d9bfde177
Akhilvv97 <72017891+Akhilvv97@users.noreply.github.com>
2020-10-09 15:47:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
 import React from 'react';
+
+
+import Login from './src/Login' ;
 import {Text, View, StyleSheet} from 'react-native';
 import {Properties} from './src/config/properties';
 
+
+
+
 const Foodie = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Foodie app</Text>
+    <View>
+      
+     
+      <Login/>
     </View>
   );
 };
 export default Foodie;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Properties.color.black,
-  },
-  text: {
-    color: Properties.color.green,
-    fontSize: 20,
-    fontFamily: Properties.fonts.default,
-  },
-});
+
