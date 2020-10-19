20010a5b7c2a9dffb4314c5fa01721e1e45f6191
Teslinrose <71651896+Teslinrose@users.noreply.github.com>
2020-10-19 14:15:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {Properties} from './src/config/properties';
-
-const Foodie = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Foodie app</Text>
-    </View>
-  );
-};
-export default Foodie;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Properties.color.black,
-  },
-  text: {
-    color: Properties.color.green,
-    fontSize: 20,
-    fontFamily: Properties.fonts.default,
-  },
-});
+ import React,{ Component }from 'react';
+ import {NavBarCustom}from './src/NavBarCustom';
+import Homescreen from './src/homescreen';
+import Myorders from './src/myorders';
+import Wallet from './src/Wallet';
+import Login from './src/Login';
+import Support from './src/support';
+import IPL_Challenge from './src/IPL_Challenge';
+import Profile from './src/Profile';
+import Challenge_Winners from './src/Challenge_Winners';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+const Drawer = createDrawerNavigator();
+export default class app extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Drawer.Navigator drawerContent={props => <NavBarCustom {...props} />}
+         drawerStyle={{
+          width:320,
+        }}>
+          <Drawer.Screen name="Profile" component={Profile}/> 
+          <Drawer.Screen name="Home" component={Homescreen}/> 
+          <Drawer.Screen name="My Orders" component={Myorders} />
+          <Drawer.Screen name="Wallet" component={Wallet} />
+          <Drawer.Screen name="Account" component={Login} />
+          <Drawer.Screen name="Support" component={Support} />
+          <Drawer.Screen name="IPL Challenge" component={IPL_Challenge} />
+          <Drawer.Screen name="IPL Challenge Winners" component={Challenge_Winners} />
+          </Drawer.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
