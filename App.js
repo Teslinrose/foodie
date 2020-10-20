960bf8ed49accad3d09ecd7b5ff5d007f6085ac7
Teslinrose <71651896+Teslinrose@users.noreply.github.com>
2020-10-20 12:14:46 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,8 +6,13 @@ import Wallet from './src/Wallet';
 import Login from './src/Login';
 import Support from './src/support';
 import IPL_Challenge from './src/IPL_Challenge';
-import Profile from './src/Profile';
 import Challenge_Winners from './src/Challenge_Winners';
+import MY_Profile from './src/MY_Profile';
+import Location from './src/Location';
+import Subscribe from './src/Subscribe';
+import Signin from './src/Signin';
+import Forgot from './src/Forgot';
+import Splashscreen from './src/Splashscreen';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 const Drawer = createDrawerNavigator();
@@ -19,14 +24,21 @@ export default class app extends Component {
          drawerStyle={{
           width:320,
         }}>
-          <Drawer.Screen name="Profile" component={Profile}/> 
+          
           <Drawer.Screen name="Home" component={Homescreen}/> 
           <Drawer.Screen name="My Orders" component={Myorders} />
           <Drawer.Screen name="Wallet" component={Wallet} />
-          <Drawer.Screen name="Account" component={Login} />
+          <Drawer.Screen name="Account" component={MY_Profile} />
           <Drawer.Screen name="Support" component={Support} />
           <Drawer.Screen name="IPL Challenge" component={IPL_Challenge} />
           <Drawer.Screen name="IPL Challenge Winners" component={Challenge_Winners} />
+          <Drawer.Screen name="Profile" component={MY_Profile}/> 
+          <Drawer.Screen name="Location" component={Location}/> 
+          <Drawer.Screen name="Subscribe" component={Subscribe}/>
+          <Drawer.Screen name="Signin" component={Signin}/> 
+          <Drawer.Screen name="Forgot" component={Forgot}/>
+          <Drawer.Screen name="Login" component={Login}/>
+          <Drawer.Screen name="Splashscreen" component={Splashscreen}/>
           </Drawer.Navigator>
       </NavigationContainer>
     );
